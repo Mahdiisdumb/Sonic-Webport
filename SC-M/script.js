00a1fdc91a9e89403147176df1c928fcea500602
Mahdiisdumb <mahdihamade6789010@outlook.com>
2026-09-14 17:32:11 -0400

    fds

--- SC-M/script.js
+++ SC-M/script.js
@@ -1,4 +1,4 @@
 EJS_player = "#game";
 EJS_core = "segaCD";
-EJS_gameUrl = "SonicCD++_(J).cue";
+EJS_gameUrl = "SonicCD++_(J).iso";
 EJS_pathtodata = "./data/";
--- SC/script.js
+++ SC/script.js
@@ -1,4 +1,4 @@
 EJS_player = "#game";
 EJS_core = "segaCD";
-EJS_gameUrl = "Sonic CD (JAP).cue";
+EJS_gameUrl = "Sonic CD (JAP).iso";
 EJS_pathtodata = "./data/";
